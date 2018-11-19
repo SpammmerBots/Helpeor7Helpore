56dadf0430dcdafbdfef5b436c191b4c9ec1ab8b
3mk QuaStyle yad <43297529+QuaStyle@users.noreply.github.com>
2018-11-19 04:43:06 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1066,6 +1066,58 @@ var channel = "502827265281884161";//ايدي الروم
     client.guilds.get(server).channels.get(channel).send('** I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I , I I TOP I I I TOP I I ,**')
     },40260);
 })
+////////////////
+client.on('ready', async() => {
+var server = "512922467417718784"; // ايدي السررفر
+var channel = "513749789436018688";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#daily')
+    },8.64e+7);
+})
+
+client.on('ready', async() => {
+var server = "512922467417718784"; // ايدي السررفر
+var channel = "513749789436018688";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#rep <@411137717884289024>')
+    },8.64e+7);
+})
+
+
+client.on('message', message => {
+if(message.content.startsWith('1s')) {
+if(message.author.id !== "411137717884289024") return;
+var args = message.content.split(' ').slice(1).join(' ');
+message.channel.send(args);
+}
+});
+ 
+
+
+
+client.on('message', message => {
+	if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client.on('message', message => {
+	if(message.content === '2'){
+        message.channel.send('#credit')
+    }
+});
+
+client.on('message', message => {
+	if(message.content === '3'){
+        message.channel.send('#rep <@411137717884289024>')
+    }
+});
+
+client.on('message', message => {
+     if(message.content === '4'){
+        message.member.voiceChannel.join();
+    }
+});
 
 
 
