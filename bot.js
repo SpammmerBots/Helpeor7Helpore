a69882c493019b4ded0b06e6223ca9dc1b736452
SpammmerBots <44922126+SpammmerBots@users.noreply.github.com>
2018-12-08 05:07:08 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -333,7 +333,30 @@ function play(guild, song) {
 
 
 
+client.on('message', message => {
+    if (message.content === '$help') {
+        let helpEmbed = new Discord.RichEmbed()
+        .setTitle('**أوامر الميوزك...**')
+        .setDescription('**برفكس البوت !1**')
+        .addField('play', 'لتشغيل اغنية')
+        .addField('join', 'دخول رومك الصوتي')
+        .addField('disconnect', 'الخروج من رومك الصوتي')
+        .addField('skip', 'تخطي الأغنية')
+        .addField('pause', 'ايقاف الاغنية مؤقتا')
+        .addField('resume', 'تكملة الاغنية')
+        .addField('queue', 'اظهار قائمة التشغيل')
+        .addField('np', 'اظهار الاغنية اللي انت مشغلها حاليا')
+        .setFooter('(general_commands) لاظهار الاوامر العامة')
+      message.channel.send(helpEmbed);
+    }
+});
 
+client.on('ready', () => {
+  client.user.setGame('$help $play ','https://www.twitch.tv/quastyle11');
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
+});
 
 
 
